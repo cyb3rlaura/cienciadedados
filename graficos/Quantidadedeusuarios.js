23a262063392a5dc5cf57915788ae826a0584a33
laurinha <114171279+cyb3rlaura@users.noreply.github.com>
2024-10-17 09:35:31 -0300

    Quantidadedeusuarios.js

--- graficos/Quantidadedeusuarios.js
+++ graficos/Quantidadedeusuarios.js
@@ -1,3 +1,5 @@
+import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeUsuarios() {
   const url = ‘https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json’
   const res = await fetch(url)
@@ -10,8 +12,40 @@ async function quantidadeUsuarios() {
       x: nomeDasRedes,
       y:quantidadeDeUsuario,
       type: 'bar'
+      marker: {
+         color: getCSS('--primary-color')
+  }
     }
 ]
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários',
+      x: 0,
+       font: {
+         color: getCSS('--primary-color'),
+         size: 30,
+         font: getCSS('--font')
+       }
+    },
+    xavis: {
+      tickfont: tickConfig,
+      title: {
+        text: 'Nome das redes'
+        font: {
+          color: getCSS('secundary-color')
+         }
+   }
+   },
+
+yaxis: {
+  tickfont: tickConfig,
+    title: {
+    text: 'Bilhões de usuário ativos',
+      font: {
+      color: getCSS('--secundary-color')
 
 const graficos = document.createElement('div')
   grafico.className = 'graficos'
